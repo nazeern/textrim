df7ba3cccc660bab288cfa16501b265f34ec1aaf
Nitin Nazeer <nitin.nazeer@gmail.com>
2024-10-19 14:28:04 -0400

    autodeploy, docker build & push

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,8 @@
 export default async function Landing() {
-  return <p>Hello! This is a landing page, I guess.</p>;
+  return (
+    <p>
+      Hello! This is a landing page, I guess. And hi, this line is from an
+      automated deployment!
+    </p>
+  );
 }
