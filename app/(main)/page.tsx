48c115a6652e2beef7004b2b203e83cfce5cb744
Nitin Nazeer <nitin.nazeer@gmail.com>
2024-10-23 21:36:11 -0400

    unused userId var, directions on landing

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,16 @@
 export default async function Landing() {
   return (
-    <p>
-      Hello! This is a landing page, I guess. And hi, this line is from an
-      automated deployment! And Im from a Github Workflow!
-    </p>
+    <div>
+      <p>
+        Hello! This is a landing page, I guess. And hi, this line is from an
+        automated deployment! And Im from a Github Workflow!
+      </p>
+      <p>
+        A good example (link):{" "}
+        <a href="https://supabase.com/">Supabase Landing Page</a>
+        <br />
+        But with the blue color scheme
+      </p>
+    </div>
   );
 }
--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -20,7 +20,7 @@ export default async function ProjectsDashboard() {
     <div className="px-1 max-w-4xl w-full flex flex-col gap-y-4">
       <p className="font-bold text-3xl">Your Projects</p>
       <AddProject userId={user.id} hasNoProjects={!projects.length} />
-      <ProjectsList userId={user.id} projects={projects} />
+      <ProjectsList projects={projects} />
     </div>
   );
 }
